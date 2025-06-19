d1b83e81db389d4dc3a77d79243b4a1c95950861
marcellotc <coletti.marcello@gmail.com>
2025-06-18 22:39:39 -0400

    Nueva estructura para la nomina y workplaces

--- init-db/02_business_entities/09_workplaces.sql
+++ init-db/02_business_entities/09_workplaces.sql
@@ -1,4 +1,14 @@
 CREATE TABLE Workplaces (
-    WorkplaceID VARCHAR(20) PRIMARY KEY,              -- e.g. unique ID for workplace
-    WorkplaceType ENUM('Store', 'Warehouse') NOT NULL -- Must be 'Store' or 'Warehouse'
+    WorkplaceID VARCHAR(20) PRIMARY KEY,    -- e.g. unique ID for workplace
+    WorkplaceType VARCHAR(10) NOT NULL,     -- Must be 'Store' or 'Warehouse',
+    Store VARCHAR(10) NOT NULL,     -- Must be 'Store' or 'Warehouse' from their own tables,
+    Warehouse VARCHAR(10) NOT NULL,
+
+    CONSTRAINT CHK_Workplace_Type CHECK ( WorkplaceType IN ('Store', 'Warehouse') ),
+
+    CONSTRAINT FK_Workplace_Store FOREIGN KEY (Store)
+        REFERENCES Stores(StoreID),
+
+    CONSTRAINT FK_Workplace_Warehouse FOREIGN KEY (Warehouse)
+        REFERENCES Warehouses(WarehouseID)
 );
--- init-db/99_master_init.sql
+++ init-db/99_master_init.sql
@@ -15,23 +15,21 @@ SOURCE ./init-db/01_core_tables/06_building_type_minimum_employees.sql;
 
 -- Business entities
 SOURCE ./init-db/02_business_entities/04_warehouses.sql;
-
-SOURCE ./init-db/02_business_entities/09_workplaces.sql;
-
--- Payroll
-SOURCE ./init-db/04_payroll/01_payroll.sql;
-
--- Business entities
-
 SOURCE ./init-db/02_business_entities/01_stores.sql;
 SOURCE ./init-db/02_business_entities/02_store_prices.sql;
-SOURCE ./init-db/02_business_entities/03_store_workers.sql;
-
 SOURCE ./init-db/02_business_entities/05_warehouse_prices.sql;
 SOURCE ./init-db/02_business_entities/06_warehouse_min_sell_qty.sql;
-SOURCE ./init-db/02_business_entities/07_warehouse_workers.sql;
 SOURCE ./init-db/02_business_entities/08_warehouse_stores.sql;
 
+-- Payroll
+
+-- store_workers (workplace_id, store) -> general Workplace (workplace_id, Ocupado o no, esencial o no, charge) -> payroll (with workplace id)
+-- warehouse_workers (workplace_id, warehouse) -> general Workplace (workplace_id, Ocupado o no, esencial o no, charge) -> payroll (with workplace id)
+
+SOURCE ./init-db/02_business_entities/03_store_workers.sql;
+SOURCE ./init-db/02_business_entities/07_warehouse_workers.sql;
+SOURCE ./init-db/02_business_entities/09_workplaces.sql;
+SOURCE ./init-db/04_payroll/01_payroll.sql;
 
 -- Stock tables
 SOURCE ./init-db/03_stock_tables/01_store_stock.sql;
